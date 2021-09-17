046b84f82ba9b068b551bd78ff921f9d4a7d1c32
sararekowska <sara.rekowska@gmail.com>
2021-09-18 00:16:14 +0200

    fix: double clicks

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
 
-const insert = (board, x1, y1, value) =>
-  board.map((row, x2) => row.map((item, y2) => (x1 == x2 && y1 == y2 ? value : item)));
+const insert = (board, x1, y1, value, setCount) =>
+  board.map((row, x2) =>
+    row.map((item, y2) => {
+      if (x1 === x2 && y1 === y2 && item === "_") {
+        setCount((prev) => prev + 1);
+        return value;
+      } else {
+        return item;
+      }
+    })
+  );
 
 const Board = () => {
   const [board, setBoard] = useState([
     ["_", "_", "_"],
     ["_", "_", "_"],
-    ["_", "_", "_"]
+    ["_", "_", "_"],
   ]);
   const [count, setCount] = useState(0);
   const handleClick = (x, y) => {
-    setCount(count + 1);
-    const element = count % 2 == 0 ? "x" : "o";
-    setBoard(insert(board, x, y, element));
+    const element = count % 2 === 0 ? "x" : "o";
+    setBoard(insert(board, x, y, element, setCount));
   };
   return (
     <table>
