6e1e88b878d609d1696676ac84ff30e193b93997
sararekowska <sara.rekowska@gmail.com>
2021-09-18 11:15:48 +0200

    feat: partially implement win checking

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
 
-const insert = (board, x1, y1, value, setCount) =>
-  board.map((row, x2) =>
-    row.map((item, y2) => {
-      if (x1 === x2 && y1 === y2 && item === "_") {
-        setCount((prev) => prev + 1);
-        return value;
-      } else {
-        return item;
-      }
-    })
-  );
+const checkWin = (board, count) => {
+  const element = count % 2 === 0 ? "o" : "x";
+
+  if (
+    board[0][0] === board[1][1] &&
+    board[1][1] === board[2][2] &&
+    board[0][0] !== "_"
+  ) {
+    return element;
+  } else if (
+    board[2][0] === board[1][1] &&
+    board[2][0] === board[0][2] &&
+    board[2][0] !== "_"
+  ) {
+    return element;
+  } else {
+    return false;
+  }
+};
 
 const Board = () => {
   const [board, setBoard] = useState([
@@ -19,20 +27,38 @@ const Board = () => {
     ["_", "_", "_"],
   ]);
   const [count, setCount] = useState(0);
+  const win = checkWin(board, count);
+
+  const insert = (x1, y1, value) =>
+    board.map((row, x2) =>
+      row.map((item, y2) => {
+        if (x1 === x2 && y1 === y2 && item === "_") {
+          setCount((prev) => prev + 1);
+          return value;
+        } else {
+          return item;
+        }
+      })
+    );
+
   const handleClick = (x, y) => {
     const element = count % 2 === 0 ? "x" : "o";
-    setBoard(insert(board, x, y, element, setCount));
+    setBoard(insert(x, y, element));
   };
+
   return (
-    <table>
-      {board.map((row, x) => (
-        <tr>
-          {row.map((item, y) => (
-            <td onClick={() => handleClick(x, y)}>{item}</td>
-          ))}
-        </tr>
-      ))}
-    </table>
+    <div>
+      <table>
+        {board.map((row, x) => (
+          <tr>
+            {row.map((item, y) => (
+              <td onClick={() => handleClick(x, y)}>{item}</td>
+            ))}
+          </tr>
+        ))}
+      </table>
+      {win !== false ? <h2>wygral {win}</h2> : " "}
+    </div>
   );
 };
 
